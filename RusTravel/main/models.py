1e700da50dfaee400c7d722454cdd1ec64ec9190
levab <lev.it.up0@gmail.com>
2024-06-07 00:12:49 +0500

    Добавление таблиц в базу данных и в

--- RusTravel/main/models.py
+++ RusTravel/main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Account(models.Model):
+    username = models.CharField(max_length=30, unique=True, blank=False)
+    email = models.EmailField(max_length=50, unique=True, blank=False)
+    phone = models.CharField(max_length=15, unique=True, blank=False)
+    password = models.CharField(max_length=100, blank=False)
+
+    def __str__(self):
+        return self.username + " " + self.email
