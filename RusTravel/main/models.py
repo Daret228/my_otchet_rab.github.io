a128fc52030ab48c797f87a57e6fe68d99e223ff
Dashttps <darianokhrinaa@mail.ru>
2024-06-07 00:16:30 +0500

    Работа с базой данных для страницы отзывов

--- RusTravel/main/models.py
+++ RusTravel/main/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class UsersFeedback (models.Model):
+    username = models.CharField(max_length=30, blank=False)
+    email = models.CharField(max_length=30, blank=False)
+    feedback = models.CharField(max_length=500, blank=False)
