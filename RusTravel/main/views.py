4207a28ff6045f0e301abe933a4fd653267a0c93
levab <lev.it.up0@gmail.com>
2024-06-07 22:53:46 +0500

    принял измемнения

--- RusTravel/main/views.py
+++ RusTravel/main/views.py
@@ -2,7 +2,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.models import User
-from .forms import AccountForm
+from .forms import AccountForm, UsersFeedbackForm
+
 
 
 def index(request):
@@ -35,8 +36,45 @@ def index(request):
 
 
 def feedback(request):
+<<<<<<< HEAD
+
+    form = UsersFeedbackForm()
+
+    data = {
+        'form': form}
+
+    return render(request, 'feedback.html', data)
+
+# def register(request):
+#     if request.method == 'POST':
+#         form = RegistrationForm(request.POST)
+#         if form.is_valid():
+#             name = form.cleaned_data['name']
+#             email = form.cleaned_data['email']
+#             phone = form.cleaned_data['phone']
+#             password = form.cleaned_data['password']
+#             password_confirm = form.cleaned_data['password_confirm']
+
+#             if password == password_confirm:
+#                 if User.objects.filter(email=email).exists():
+#                     messages.error(request, 'Пользователь с таким email уже существует')
+#                 elif User.objects.filter(username=phone).exists():
+#                     messages.error(request, 'Пользователь с таким номером телефона уже существует')
+#                 else:
+#                     user = User.objects.create_user(username=phone, password=password, email=email, first_name=name)
+#                     user.save()
+#                     messages.success(request, 'Регистрация успешна')
+#                     return redirect('index')
+#             else:
+#                 messages.error(request, 'Пароли не совпадают')
+#     else:
+#         form = RegistrationForm()
+
+#     return render(request, 'index.html', {'form': form})
+=======
     return render(request, 'feedback.html')
     
+>>>>>>> 8169530c22626ed0336631c10cbc5187b30d977e
 
 
 # def login_view(request):
